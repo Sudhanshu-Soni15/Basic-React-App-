cc57d4f9c655ff31b5b3d62344d9b948c347530d
Sudhanshu-Soni15 <sudhanshusoni2002@gmail.com>
2025-03-01 20:31:16 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Sudhanshu-Soni15/Basic-React-App-.git/",
+  base: "/Basic-React-App-/",
   plugins: [react()],
 });
