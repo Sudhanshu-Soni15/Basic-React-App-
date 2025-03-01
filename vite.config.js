6453e60b07834e2ba079b718f794b27a4cff8f8a
Sudhanshu-Soni15 <sudhanshusoni2002@gmail.com>
2025-03-01 20:13:42 +0530

    changes in package.json file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: "/https://github.com/Sudhanshu-Soni15/Basic-React-App-.git/",
+  plugins: [react()],
 });
